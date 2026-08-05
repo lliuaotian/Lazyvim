bf0fe7064e3ff74cbdb5fe7d490a6aaf96d90d3a
lliuaotian <liuaotianmsj@gmail.com>
2026-08-05 17:02:03 +0800

    FEAT:禁止<CR>选择补全，很恶心很恶心

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,6 +5,15 @@ return {
     -- 我个人非常不喜欢buffer和代码片段补全
     opts.sources = opts.sources or {}
     opts.sources.default = { "lsp", "path" }
+
+    -- 关闭enter自动选中第一个补全，真的很恶心
+    opts.completion = opts.completion or {}
+    opts.completion.list = opts.completion.list or {}
+    opts.completion.list.selection = opts.completion.list.selection or {}
+    opts.completion.list.selection.preselect = false
+    opts.completion.list.selection.auto_insert = false
+    opts.keymap = opts.keymap or {}
+    opts.keymap["<CR>"] = nil
     return opts
   end,
 }
